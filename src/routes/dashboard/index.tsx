605a7018a01735b0e549a7d62935ecf7a0c41446
tigawanna <denniskinuthiawaweru@gmail.com>
2025-02-10 13:01:57 +0300

    refactor: update dashboard route to use useRouteContext and useIsographEnvironment, enhance ViewerComponent integration

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,11 +1,19 @@
-import { createFileRoute, create } from '@tanstack/react-router'
+import {
+  createFileRoute,
+  useRouteContext,
+} from '@tanstack/react-router'
+
 import { ViewerComponent } from './-components/dashoboard-sidebar/viewer/ViewerRoute'
+import { useIsographEnviroment } from '@/lib/isograph/client'
+
 
 export const Route = createFileRoute('/dashboard/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const { PAT } = useRouteContext({ from: '__root__' })
+  const environment = useIsographEnviroment(PAT!)
   return (
     <div className="min--screen flex justify-center items-center gap-2 p-2">
       <ViewerComponent />
